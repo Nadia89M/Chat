f21bf9ffc94691c3199992889b9669a5e0a8ef6a
Nadia <nadia.mohamed89@gmail.com>
2018-11-04 11:43:52 +0000

    Generate User, Chatroom, ChatroomUser and Message

---
+++ app/models/chatroom.rb
@@ -0,0 +1,4 @@
+class Chatroom < ApplicationRecord
+  has_many :users, through: :chatroom_users
+  has_many :messages
+end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  #:confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  has_many :chatrooms, through: :chatroom_users
+  has_many :messages
+end
