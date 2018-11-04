c0db82c8bec626bc781ee40f7dea26cc80dd9c51
Nadia <nadia.mohamed89@gmail.com>
2018-11-04 16:35:40 +0000

    Chatroom User Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
+
 Rails.application.routes.draw do
-  resources :chatrooms
   devise_for :users
-  root to: 'chatrooms#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :chatrooms do
+    resource :chatroom_users
+    resources :messages
+  end
+
+  resources :direct_messages
+
+  root to: "chatroom#index"
 end
